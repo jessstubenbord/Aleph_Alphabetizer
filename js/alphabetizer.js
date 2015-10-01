22e339b2a864f688eb062988b06f0eba7cf77e7a
Jess Stubenbord <git@jess.pro>
2015-10-01 14:57:34 +0200

    Release v00.01

--- js/alphabetizer.js
+++ js/alphabetizer.js
@@ -1,15 +1,3 @@
-//basis of alphabetization code from
-//http://stackoverflow.com/a/25431980
-//http://stackoverflow.com/a/9645447
-
-// still need to add:
-//clearing all items
-//more things
-
-//string logic
-///tolowercase
-///check for returns
-
 
 var alphabetizeOn = true;
 var instantEntry = true;
@@ -38,13 +26,13 @@ function shuffleArray(array) {
 };
 
 ///check if there are stored items from a previous session and load them into items
+//not currently used
 var replaceSavedItems = function(){
 	if (savedItems === true) {
 		items = savedItems;
 	};
 };
 
-//check if in multi-entry mode
 
 
 //display items according to preset options
@@ -52,68 +40,61 @@ var displayItems = function() {
 
     var $list = $('.list');       
 
-	if (alphabetizeOn === true) {
+	if (alphabetizeOn === true) {                                   //if alphabetize is on
 		items.sort(function (a, b) {
             return a.toLowerCase().localeCompare(b.toLowerCase());
-        });         						//sort items
-        $('.alphabet-box').prop( 'checked', true );
+        });                                                         //sort items independent of case
+        $('.alphabet-box').prop( 'checked', true );                 //make sure the option is checked
 	};
-	if (shuffleData === true ) {
-        alphabetizeOn = false;              //turn off auto-alphabetize
-		shuffleArray(items);				//shuffle data
-        $('.alphabet-box').prop( 'checked', false );
-        $('.shuffle-box').prop( 'checked', true );
-		if (alwaysShuffleData === false) {
-			//shuffleData = false;			//don't shuffle it next time
-            //$('.shuffle-box').prop( 'checked', false );
-		};
+	if (shuffleData === true ) {                                    //if shuffle is on
+        alphabetizeOn = false;                                      //turn off auto-alphabetize
+		shuffleArray(items);				                        //shuffle data
+        $('.alphabet-box').prop( 'checked', false );                //uncheck alphabetize
+        $('.shuffle-box').prop( 'checked', true );                  //check shuffle box
 	};
-    if (shuffleData === false) {
-        $('.shuffle-box').prop( 'checked', false );
+    if (shuffleData === false) {                                    //if shuffle is off
+        $('.shuffle-box').prop( 'checked', false );                 //uncheck shuffle box
     };
-	if (multiEntry === true) {
+	//if (multiEntry === true) {
 		//only fires if it's true from the start, not if it's fired by key recognition
-	}
-	if (deleteOnClick === true) {
-		$list.addClass("deletable");
-		deleteFunction();//run the delete function
+	//}
+	if (deleteOnClick === true) {                                   //if deletable is on
+		$list.addClass("deletable");                                //make things appear deletable
+		deleteFunction();                                           //run the delete function
 		//figure out how to stop the delete function
 	};
 
-    $list.html('');                            // clear the last content of .word-list
-    $.each(items, function (indexPosition) {                // iterate through the words
-        $list.append('<li class="' + indexPosition + '">' + this + '</li>'); // append a list item for each word (this refers to the word in the array
+    $list.html('');                                                 // clear the last content of .list
+    $.each(items, function (indexPosition) {                        // iterate through the list
+        $list.append('<li class="' + indexPosition + '">' + this + '</li>'); // append a list item for each
     });
 };
 
 
 //grab items from input
 $('.css-input').on('keyup', function (event) {              // listen to each keypress in the input box
-    if (event.which === 13 && !event.shiftKey) {            // if enter is pressed
+    if (event.which === 13 && !event.shiftKey) {            // if just enter is pressed
         var $input = $(event.target);                       // take the reference to the inputbox
         var item = $input.val();                            // take the value of the input box 
         var itemTrimmed = item.trim();                      // trim leading and following white space
-        //var itemsSplit = itemTrimmed.split(/\r\n|\r|\n/g);
-        items.push(itemTrimmed);                            // add it to the array
+        var itemsSplit = itemTrimmed.split(/\r\n|\r|\n/g);  // split at each line break
+        $.each(itemsSplit, function (indexPosition){        // take each split item
+            items.push(itemsSplit[indexPosition]);          // add it to the array
+        });
         $input.val('');                                     // clear the input box
-
-        //displayItems();                                   /// show the words
     }
     if (event.which === 13 && event.shiftKey) {             // if enter and shift are pressed
-    	$('.css-input').addClass('multi-entry');			//Activate multi-entry mode
-    	multiEntry = true;
+    	$('.css-input').addClass('multi-entry');			// Activate multi-entry mode
+    	multiEntry = true;                                  // switch it on
     }
     if (event.which === 27) {                               // if escape is pressed
-        $('.css-input').removeClass('multi-entry');         //de-activate multi-entry mode
-        multiEntry = false;
+        $('.css-input').removeClass('multi-entry');         // de-activate multi-entry mode
+        multiEntry = false;                                 // switch it off
         var $input = $(event.target);                       // take the reference to the inputbox
         $input.val('');                                     // clear the input box
     }
-
-    else{
-    	displayItems();
-    }
-
+    
+    displayItems();                                         // display all of the items
 
 });
 
@@ -123,7 +104,7 @@ var deleteFunction = function(){
         $('ul.list').on('click', 'li', function () {    //on clicking a list item
             var arrayPosition = $(this).attr('class');  //grab the array position from the class
             items[arrayPosition] = " ";                 //replace it with a blank space
-            $(this).addClass('inert');
+            $(this).addClass('inert');                  //make it look inert
             //$(this).empty(); //make it dissapear
             //$(this).html('<a href="#undid">undo</a>');//add undo link
             //remove from array
@@ -136,37 +117,41 @@ var deleteFunction = function(){
 };
 
 //option Checkbox actions
-$(".deletable-box").change(function() {
-    if(this.checked) {
-        deleteOnClick = true;
+$(".deletable-box").change(function() {     // if deletable is toggled
+    if(this.checked) {                      // on
+        deleteOnClick = true;               // switch deletable on
     }
-    else{
-        deleteOnClick = false;
+    else{                                   // off
+        deleteOnClick = false;              // switch deletable off
     }
-    displayItems();
+    displayItems();                         // display items
 });
-$(".alphabet-box").change(function() {
-    if(this.checked) {
-        alphabetizeOn = true;
-        shuffleData = false;
+$(".alphabet-box").change(function() {      // if alphabetize is toggled
+    if(this.checked) {                      // on
+        alphabetizeOn = true;               // switch alphabetize on
+        shuffleData = false;                // switch shuffle off
     }
-    else{
-        alphabetizeOn = false;
+    else{                                   // off
+        alphabetizeOn = false;              // switch alphabetize off
     }
-    displayItems();
+    displayItems();                         // display items
 });
-$(".shuffle-box").change(function() {
-    if(this.checked) {
-        shuffleData = true;
-        alphabetizeOn = false
+$(".shuffle-box").change(function() {       // if shuffle is toggled
+    if(this.checked) {                      // on
+        shuffleData = true;                 // switch shuffle on
+        alphabetizeOn = false;              // switch alphabetize off
     }
-    else{
-        shuffleData = false;
+    else{                                   // off
+        shuffleData = false;                // switch shuffle off
     }
-    displayItems();
+    displayItems();                         // display items
 });
-$(".clear-button").click(function(){
-    items.length = 0;   // clear all data in items array
-    displayItems();
+$(".clear-button").click(function(){        // if clear button is clicked
+    items.length = 0;                       // clear all data in items array
+    displayItems();                         // display items
 });
 
+
+//some alphabetization code repurposed from
+//http://stackoverflow.com/a/25431980
+//http://stackoverflow.com/a/9645447
