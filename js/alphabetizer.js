1607bafd5840932c312217ec75468f59c7efa154
Jess Stubenbord <git@jess.pro>
2015-10-28 02:06:48 +0000

    Repaired item removal
    CSS:
    converted custom font sizes to rem
    changed items li spacing to highlight overflowing text entries
    
    JS
    Code cleanup
    Fixed how items in array are counted
    Added fade-out animation on delete of items

--- js/alphabetizer.js
+++ js/alphabetizer.js
@@ -100,23 +100,22 @@ var displayItems = function() {
         deleteFunction();                                           //run the delete function
     };
 
-// start making the list
+    // start making the list
 
-    $list.html('');                                                 // clear the last content of .list
+    $list.html('');                                                 // clear the previous contents of .list
 
     $.each(items, function (indexPosition) {                        // iterate through the list
         $list.append('<li class="' + indexPosition + '">' + this + '</li>'); // append a list item for each
     });
 
-// highlite active entry** -note to self, learn how to actually spell highlite 
-
-    mostRecentItemPosition = items.indexOf(mostRecentItem);             // sets most recent item position so that it can be highlited in list *not scoped correctly
+    // highlite active entry** -note to self, learn how to actually spell highlite 
+    mostRecentItemPosition = items.indexOf(mostRecentItem);             // sets most recent item position so that it can be highlited in list
     if (activeEntry === true) {                                         // if active entry is on
         $(listHtmlLocation + ' .' + mostRecentItemPosition).addClass('active');  // make active item visible
     };
 
 
-//start scrolling the list to current item
+    //start scrolling the list to current item
 
     var scrollToPosition = mostRecentItemPosition;                          // set the target to scroll to
 
@@ -133,23 +132,28 @@ var displayItems = function() {
         }, 500);             
     };
 
-
-//    $(listContainerHtmlLocation).scrollTop($('li.' + scrollToPosition).position().top);  // scroll to most recent item non-animated
+    //$(listContainerHtmlLocation).scrollTop($('li.' + scrollToPosition).position().top);  // scroll to most recent item non-animated
 };
 
 
 
 //grab items from input
 //This segment listens to and manipulates the input box... and does some stuff with what it grabs
+/*
+How it works:
+Im typing > each keyup fires logic > if I haven't hit enter yet, I want the item to be deleted every time
+If I have hit enter > I want the item to be added to the array
+items in array keeps track of number of permanent items
+*/
+$(inputHtmlLocation).on('keyup', function (event) {     // listen to each keypress in the input box
 
-$(inputHtmlLocation).on('keyup', function (event) {              // listen to each keypress in the input box
-
-    if (items.length > itemsInArray ) {                 // if an item has been added to the array temporarily
-        if (mostRecentItemPosition > -1) {
-            items.splice(mostRecentItemPosition, 1);    // delete it now
+    if (itemsInArray < items.length) {                  // if (permanent) items in array is less than the current # of items
+        if (items.length >= 0) {                        // and if there are items there
+            items.splice(mostRecentItemPosition, 1);    // delete the last thing that was put in
+            itemsInArray = items.length;                // reset the number of items (just in case)
+            //console.log('removed: ' + mostRecentItem + ' from: ' + mostRecentItemPosition); //
         };
     };
-
 
     var $input = $(event.target);                       // set $input as the source of all of this
     var item = $input.val();                            // take the value of the input box 
@@ -160,24 +164,16 @@ $(inputHtmlLocation).on('keyup', function (event) {              // listen to ea
 
 
     // special cases for certain keys/combos
-
-    if ( (event.which === 13 || event.which === 10) && !event.shiftKey) {   // if enter is pressed
-        $input.val(' ');                                                    // empty the input (space prevents placeholder text from displaying)
-        itemsInArray++;                                                     // add one to the item counter
-        //console.log('enter');
-        $(listHtmlLocation + ' .' + mostRecentItemPosition).removeClass('active');  // remove active class from now inactive item
-        activeEntry = false;                                                        // set active to off
-    };
-
+    //Shift-enter & ctrl-v
     if (event.which === 13 && event.shiftKey || event.which === 86 && (event.ctrlKey || event.metakey)) {     // if shift and enter or ctrl v are pressed
-    	$(inputHtmlLocation).addClass('multi-entry');			// Activate multi-entry mode
+    	$(inputHtmlLocation).addClass('multi-entry');       // Activate multi-entry mode
         $('.hint-box').removeClass('default');              // de-ctivate default hint
         $('.hint-box').addClass('multi');                   // Activate multi-entry hint
 
     	multiEntry = true;                                  // switch multi entry on
         instantEntry = false;                               // turn off instant entry
     }
-
+    //escape
     if (event.which === 27) {                               // if escape is pressed
         $(inputHtmlLocation).removeClass('multi-entry');    // de-activate multi-entry mode
         $('.hint-box').removeClass('multi');                // de-activate multi-entry hint
@@ -195,34 +191,36 @@ $(inputHtmlLocation).on('keyup', function (event) {              // listen to ea
             var itemsSplit = itemTrimmed.split(/\r\n|\r|\n/g);  // split at each line break
             $.merge(items, itemsSplit);                         // add split items to array
             $input.val(' ');                                    // empty the input (space prevents placeholder text from displaying)
-            itemsInArray++;                                     // increase the "official" item count
-            ///console.log('enter')
+            itemsInArray = itemsInArray + itemsSplit.length;    // increase the "official" item count
+            itemsSplit.length = 0;                              // empty 
         };
-
-        if (spaceSplit === true) {                              // unsupported, doesn't really work
-            var itemsSplit = itemTrimmed.split(/\s+/g);         // split at each whitespace
-            $.merge(items, itemsSplit);                         // add split items to array
-            $input.val(' ');                                    // empty the input (space prevents placeholder text from displaying)
-            itemsInArray++;            
-        };
-    }
-    else{                                                       //  for evrything else
-            var itemsSplit = itemTrimmed.split(/\r\n|\r|\n/g);  // split at each line break
-            $.merge(items, itemsSplit);                         // add split items to array
-    //        items.push(itemTrimmed);// just add trimed items
+    } else {                                                    //  for everything else
+        var itemsSplit = itemTrimmed.split(/\r\n|\r|\n/g);      // split at each line break
+        $.merge(items, itemsSplit);                             // add split items to array            
+        //  items.push(itemTrimmed);// just add trimed items
     }
 
-
+    //when enter is pressed
+    if ( (event.which === 13 || event.which === 10) && !event.shiftKey) {           // if enter(or line feed) is pressed
+        $input.val(' ');                                                            // empty the input (space prevents placeholder text from displaying)
+        //console.log('enter');
+        itemsInArray = itemsInArray + itemsSplit.length;                            // increase the "official" item count
+        itemsSplit.length = 0;                                                      // empty
+        $(listHtmlLocation + ' .' + mostRecentItemPosition).removeClass('active');  // remove active class from now inactive item
+        activeEntry = false;                                                        // set active to off
+    };
     
     displayItems();                                         // display all of the items
 
 });
 
+//this stops a new line from appearing in the input box
 $(inputHtmlLocation).on('keydown', function (event) {       // listen to each keydown in the input box
     if (event.which === 13 && !event.shiftKey) {            // if it's enter & not shift enter
         event.preventDefault();                             // stop enter from being entered
     };
 });
+
 
 var deleteFunction = function(){
 //delete items from array
@@ -231,12 +229,19 @@ var deleteFunction = function(){
             var arrayPosition = $(this).attr('class');      // grab the array position from the class
             items[arrayPosition] = " ";                     // replace it with a blank space
             $(this).addClass('inert');                      // make it look inert
-            //$(this).html('<a href="#undid">undo</a>');    // add undo link
+            var notQuiteThis = this;                        // hold reference for fadeout animation
+            var makeItDisappear = function(){               // animated hide of element
+                $(notQuiteThis).fadeOut({ duration: 300, queue: false });
+                $(notQuiteThis).animate({right:100},600);
+            };
+            setTimeout(makeItDisappear, 600);               //delay animation 600ms
+            
+            //$(this).append(' <span class=\"undo\">undo</a>');// add undo link
         });
     }
 };
 
-//option Checkbox actions
+//options Checkbox actions
 $(".deletable-box").change(function() {     // if deletable is toggled
     if(this.checked) {                      // on
         deleteOnClick = true;               // switch deletable on
@@ -294,7 +299,6 @@ var scrollOptions = function(chosenClass){
         forceHideOptions = false;                                                   // turn force hide off
       };
     //alert(' options have been toggled and chosenClass=' + chosenClass + 'scroll: ' + $(window).scrollTop() + ' thinDisplay: ' + thinDisplay + ' showOptions: ' + showOptions);
-
 }
 
 $('.options-container .options-head').click(function () {                           // when the options header is clicked
